22390e9f25321d9d7a980a826afa984ddf92a5e8
eunddodi <eundppojjack@gmail.com>
2022-12-05 18:33:19 +0900

    refactor: remove useless methods, add comments, reorder

--- src/core/Component.js
+++ src/core/Component.js
@@ -9,28 +9,41 @@ export class Component {
 		this.props = props;
 		this.registeredEvents = [];
 
-		this.setup();
 		this.render();
 		this.setEvent();
 		this.mounted();
 	}
-	setup() {}
+
 	mounted() {}
-	updated() {}
-	template() {
-		return '';
-	}
-	render() {
-		this.$target.innerHTML = this.template();
-		this.updated();
-	}
-	setEvent() {}
+
 	setState(newState) {
+		// 상태 업데이트 후 리렌더링 한다.
 		this.state = { ...this.state, ...newState };
 		this.render();
 	}
 
+	render() {
+		this.$target.innerHTML = this.template();
+		this.updated();
+	}
+
+	updated() {}
+
+	willUnmount() {
+		// 언마운트 전 등록된 이벤트리스너를 모두 제거한다.
+		this.registeredEvents.forEach(({ type, eventListener }) => {
+			this.$target.removeEventListener(type, eventListener);
+		});
+	}
+
+	template() {
+		return '';
+	}
+
+	setEvent() {}
+
 	addEvent(type, selector, callback) {
+		// 언마운트 시 이벤트리스너를 제거하기 위해 이벤트리스너들을 배열로 관리한다.
 		const children = [...this.$target.querySelectorAll(selector)];
 		const isTarget = (target) =>
 			children.includes(target) || target.closest(selector);
@@ -46,11 +59,5 @@ export class Component {
 			this.registeredEvents[this.registeredEvents.length - 1];
 
 		this.$target.addEventListener(type, recentlyAddedEvent.eventListener);
-	}
-
-	willUnmount() {
-		this.registeredEvents.forEach(({ type, eventListener }) => {
-			this.$target.removeEventListener(type, eventListener);
-		});
 	}
 }
